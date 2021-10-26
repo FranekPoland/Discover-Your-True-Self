b9f2beb9a619a67504bb7d42883630d66ae4d348
Franek <klocekfranciszek@gmail.com>
2021-10-26 16:20:08 +0200

    login in progress

--- webapp/modules/login.js
+++ webapp/modules/login.js
@@ -1,5 +1,8 @@
 // TODO save name and password in local storage after clicking on login, eye should work
 
+var url = 'https://rocky-shore-64084.herokuapp.com/'
+var user = {}
+
 var initLogin = function () {
     $('.btn-login').on('click', login);
     $('.register').on('click', initRegister);
@@ -10,20 +13,21 @@ var initRegister = function () {
     $('.btn-register').show();
     $('.btn-login').hide();
 }
-
 function register() {
     
     var name = $('#username').val();
     var password = $('#password').val();
-    console.log(name,password);
+    console.log(name, password);
     // $('.login-link').show();
     $('.register').hide();
     $('.btn-register').hide();
     $('.btn-login').show();
+    url = url + 'register?&name=' + name + '&password=' + password;
+    console.log('to jest url', url);
     $.ajax({
-        method: "PUT",
-        url: 'https://discover-self-default-rtdb.europe-west1.firebasedatabase.app/users/' + name + '.json',
-        data: JSON.stringify({ 'password': password }),
+        method: "POST",
+        url: url,
+        // data: JSON.stringify({ 'password': password }),
         dataType: "json"
       }).done(function(resp) {
           console.log(resp)
@@ -37,9 +41,10 @@ function login() {
     $('.notification').hide();
     var users;
     $.ajax({
-        method: "GET",
-        url: "https://discover-self-default-rtdb.europe-west1.firebasedatabase.app/users.json"
+        method: "POST",
+        url: url + 'login?name=' + name + '&password=' + password
     }).then(function( resp ) {
+        console.log(resp);
         users = resp;
         var isAuth = auth(users, name, password);
         console.log('isAuth', isAuth)
@@ -49,7 +54,8 @@ function login() {
     
 }
 
-function auth(users, name, password) {
+function auth(res) {
+    console.log(users);
     return users[name] ? users[name].password === password : false;
 }
 
